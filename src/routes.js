b16291b4ef05b7bccffba8afcb151ef8e70a5c50
TharinduH <hettipower@gmail.com>
2025-04-04 13:46:13 +0530

    route and packages updated

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 import SignInBasic from "layouts/authentication/sign-in";
 import Dashboard from "layouts/dashboard";
+import Teachers from "layouts/teachers";
+import Classes from "layouts/classes";
+import AddTeacher from "layouts/teachers/add";
+import AddSubject from "layouts/subjects/add";
+import Subjects from "layouts/subjects";
 
 // @mui icons
 import Icon from "@mui/material/Icon";
@@ -19,6 +24,46 @@ const routes = [
     icon: <Icon fontSize="medium">dashboard</Icon>,
     component: <Dashboard />,
     noCollapse: true,
+  },
+  {
+    type: "collapse",
+    name: "Teachers",
+    key: "teachers",
+    route: "/teachers",
+    icon: <Icon fontSize="medium">school</Icon>,
+    component: <Teachers />,
+    collapse: [
+      {
+        name: "Add Teacher",
+        key: "add-teacher",
+        route: "/teachers/add-teacher",
+        component: <AddTeacher />,
+      },
+    ],
+  },
+  {
+    type: "collapse",
+    name: "Classes",
+    key: "classes",
+    route: "/classes",
+    icon: <Icon fontSize="medium">class</Icon>,
+    component: <Classes />,
+    noCollapse: true,
+  },
+  {
+    type: "collapse",
+    name: "Subjects",
+    key: "subjects",
+    route: "/subjects",
+    icon: <Icon fontSize="medium">build</Icon>,
+    component: <Subjects />,
+    noCollapse: true,
+  },
+  {
+    name: "Add Subject",
+    key: "add-subject",
+    route: "/subjects/add-subject",
+    component: <AddSubject />,
   }
 ];
 
